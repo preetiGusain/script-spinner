12e50c5ad87050c6dbe6d120a2eeca94a892dccb
Preeti Gusain <preetigusain@Preetis-MacBook-Air.local>
2024-12-15 18:42:08 +0530

    Understood how setTimeout takes arguments

--- launcher.js
+++ launcher.js
@@ -2,28 +2,29 @@ import fetch from 'node-fetch';
 import dotenv from 'dotenv';
 
 dotenv.config();
-var URLs = process.env.URL.split(",");
+const url = process.env.URL.split(",");
+console.log(url);
 
-const runTask = async (URLs) => {
+const runTask = async (url) => {
     console.log("Running Node start script in every 15 minutes.");
-    for (let url of URLs) {
-        try {
-            console.log(`Sending request to: ${url}`);
-            const response = await fetch(url);
+    try {
+        console.log(`Sending request to: ${url}`);
+        const response = await fetch(url);
 
-            if (response.ok) {
-                console.log("Request successful:", response.status);
-            } else {
-                console.error("Request failed:", response.status);
-            }
-            const body = await response.text();
-            console.log(`Response received from ${url}:`, body);
-        } catch (error) {
-            console.error('Error occurred:', error.message);
+        if (response.ok) {
+            console.log("Request successful:", response.status);
+        } else {
+            console.error("Request failed:", response.status);
         }
+        const body = await response.text();
+        console.log(`Response received from ${url}:`, body);
+    } catch (error) {
+        console.error('Error occurred:', error.message);
     }
     console.log('Next task scheduled after 15 minutes.');
-    setTimeout(runTask, 15 * 60 * 1000);
+    setTimeout(runTask, 15 * 60 * 1000, url);
 }
 
-runTask(URLs);
+for(let i=0;i<url.length; i++) {
+    runTask(url[i]);
+}
